a6c2495e006bf8eeaf90de2afece99129ac756fb
Kyj0326 <qoloa0326@gmail.com>
2020-03-13 17:52:14 +0900

    security with JPA

---
+++ src/main/java/com/skcc/account/AccountService.java
@@ -0,0 +1,39 @@
+package com.skcc.account;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService implements UserDetailsService {
+
+    @Autowired
+    AccountRepository accountRepository;
+
+
+    @Override//username해당하는 account계정을 가져와서 UserDetails라는 타입으로 리턴해주는거!
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Account account = accountRepository.findByUsername(username);
+        if(account == null){
+            throw new UsernameNotFoundException(username);
+
+        }
+        //security가 제공하는 User객체를 이용해서 타입을 바꿔서 넘겨주자.
+        return User.builder()
+                .username(account.getUsername())
+                .password(account.getPassword())
+                .roles(account.getRole())
+                .build();
+    }
+
+    public Account createNew(Account account) {
+        account.encodePassword();
+        return accountRepository.save(account);
+
+    }
+
+
+}
